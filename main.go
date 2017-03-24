f2b25d72e394942a3490e09c403bb481a3d20162
Bruckner, Alexander <Alexander.Bruckner@zuehlke.com>
2017-03-24 11:45:27 +0100

    main.go cleaned up

--- main.go
+++ main.go
@@ -9,17 +9,11 @@ import (
 func main() {
 
 	var ch chan int = remote.SendIntChan("test", "127.0.0.1:8080")
+	var ch2 chan int = remote.SendIntChan("test2", "127.0.0.1:8080")
 
 	for i := 1; i <= 10; i++ {
 		fmt.Printf("SendIntChan: %v\n", i)
 		ch <- i
-		time.Sleep(1 * time.Second)
-	}
-
-	var ch2 chan int = remote.SendIntChan("test2", "127.0.0.1:8080")
-
-	for i := 1; i <= 10; i++ {
-		fmt.Printf("SendIntChan2: %v\n", i)
 		ch2 <- i
 		time.Sleep(1 * time.Second)
 	}
